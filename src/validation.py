0305929eb517fcacf45d74a54c4ff6daf3c3579f
Chen Xinyu <xinyuc@comp.nus.edu.sg>
2018-10-26 22:22:30 +0800

    validate

--- src/validation.py
+++ src/validation.py
@@ -6,32 +6,38 @@ if __name__ == '__main__':
     lig_suffix = '_lig_cg.pdb'
     pro_suffix = '_pro_cg.pdb'
     data_path = "../training_data/"
-
-    validate_smaples = 824
+    top10 = 10
+    
+    validate_smaples = 100
     filter_distance_threshold = 120;
 
-    predictions_top10 = np.zeros([validate_smaples, 10])
+
+    predictions_top10 = np.zeros([validate_smaples+1, 10+1]).astype(int)
 
 
     #iterate every protein
-    for i in range(1, validate_smaples):
+    for i in range(1, validate_smaples+1):
         pro_filename = '{:04}'.format(i) + pro_suffix
         acc_all_lig = []
+        print(i)
 
         # iterate every ligand for each protein
-        for k in range(1, validate_smaples):
+        for k in range(1, validate_smaples + 1):
             lig_filename = '{:04}'.format(k) + lig_suffix
             dist = get_distance_complex(data_path + lig_filename, data_path + pro_filename)
             if(dist < filter_distance_threshold):
-                accuarcy = 1 #accuarcy = predict ()
+                accuarcy = k #accuarcy = predict ()
                 acc_all_lig.append(accuarcy)
 
         sort_acc_idx = np.argsort(acc_all_lig)
         sort_acc_idx = sort_acc_idx[::-1]
+        print(sort_acc_idx)
 
         # only collect top 10 lig idx
-        for j in range(0, 10):
-            predictions_top10[i][j] = sort_acc_idx[j]
+        predictions_top10[i][0] = i
+        for j in range(0, min(10, len(sort_acc_idx))):
+            predictions_top10[i][j+1] = sort_acc_idx[j] + 1
 
-    np.savetxt('test.out', predictions_top10, delimiter='\t')
+    print(predictions_top10)
+    np.savetxt('test.out', predictions_top10, fmt='%i', delimiter='\t')
 
