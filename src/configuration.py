4e7018995db6850f9e1b98c5a99e4151269dbddd
Chen Xinyu <xinyuc@comp.nus.edu.sg>
2018-10-29 10:35:32 +0800

    new configuration

--- src/configuration.py
+++ src/configuration.py
@@ -2,12 +2,13 @@
 # the location of training dataset
 data_path = "../training_data/"
 
-model_file = "Trainsamples20900_Batchsize20_Errsample10_Matrixsize60_Samplesperh51100_Epoch3_model.h5"
+model_file = "v3_Trainsamples20900_Batchsize20_Errsample10_Matrixsize60_Samplesperfile1100_Epoch12_model.h5"
 
 filter_distance_threshold = 75
 
 lig_start_idx = 1
 lig_end_idx = 825
+
 
 # total 824 samples to predict
 # set parameters of validation 1
@@ -26,3 +27,26 @@ validation_3_validate_smaples = 200
 validation_4_pro_start_idx = 601
 validation_4_validate_smaples = 224
 
+
+# the last 800 samples on training dataset
+'''
+lig_start_idx = 2201
+lig_end_idx = 3001
+
+# total 800 samples to predict
+# set parameters of validation 1
+validation_1_pro_start_idx = 2201
+validation_1_validate_smaples = 200
+
+# set parameters of validation 2
+validation_2_pro_start_idx = 2401
+validation_2_validate_smaples = 200
+
+# set parameters of validation 3
+validation_3_pro_start_idx = 2601
+validation_3_validate_smaples = 200
+
+# set parameters of validation 1
+validation_4_pro_start_idx = 2801
+validation_4_validate_smaples = 200
+'''
